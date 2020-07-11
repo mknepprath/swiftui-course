18f1beb3b5f4aa3f5e559401817732d0437caa38
Michael Knepprath <mknepprath@gmail.com>
2020-07-10 22:48:41 -0400

    first commit

---
+++ day1-1.playground/Contents.swift
@@ -0,0 +1,44 @@
+import UIKit
+import PlaygroundSupport
+
+// 1. Variables
+var str = "Hello, playground"
+str = "Goodbye"
+
+// 2. Strings and integers
+var age = 38
+var population = 8_000_000
+
+// 3. Multi-line strings
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+// 4. Doubles and booleans
+var pi = 3.141
+var awesome = true
+
+// 5. String interpolation
+var score = 85
+var score_str = "Your score was \(score)"
+var score_results = "The test results are here: \(score_str)"
+
+// 6. Constants
+let taylor = "swift"
+
+// 7. Type annotations
+let string = "Hello, playground" // Type inference
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
+// 8. Summary
