68c8d2ecffe5877ec73b097e7adf00e4567c353e
Michael Knepprath <mknepprath@Enoch.attlocal.net>
2020-07-29 01:08:01 -0400

    Add Day 21

---
+++ day21-GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Michael Knepprath on 7/29/20.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
