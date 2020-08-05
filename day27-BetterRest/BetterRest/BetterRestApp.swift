24c1817879c157ea56aadf7430deb25df013e765
Michael Knepprath <mknepprath@gmail.com>
2020-08-05 01:19:18 -0400

    Add Day 27

---
+++ day27-BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Michael Knepprath on 8/5/20.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
