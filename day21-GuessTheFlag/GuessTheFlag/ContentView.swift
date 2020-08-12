fee980c0d9be322a94877f5f54820a8a831b8b0f
Michael Knepprath <mknepprath@gmail.com>
2020-08-12 19:31:23 -0400

    Add Day 34

--- day21-GuessTheFlag/GuessTheFlag/ContentView.swift
+++ day21-GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -34,6 +34,10 @@ struct ContentView: View {
     @State private var scoreTitle = ""
     @State private var scoreMessage = ""
     
+    @State private var animationAmount = 0.0
+    @State private var animationAmountWrong = 0.0
+    @State private var opacityAmount = 1.0
+    
     var body: some View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
@@ -51,10 +55,23 @@ struct ContentView: View {
                 
                 ForEach(0 ..< 3) { number in
                     Button(action: {
-                        self.flagTapped(number)
+                        withAnimation(.interpolatingSpring(stiffness: 6, damping: 6)) {
+                            self.flagTapped(number)
+                            
+                            if number == correctAnswer {
+                                self.animationAmount += 360
+                            } else {
+                                self.animationAmountWrong += 360
+                            }
+                            
+                            self.opacityAmount = 0.25
+                        }
                     }) {
                         FlagImage(country: self.countries[number])
                     }
+                    .rotation3DEffect(number == correctAnswer ? .degrees(animationAmount) : .degrees(0.0), axis: (x: 0, y: 1, z: 0))
+                    .rotation3DEffect(number != correctAnswer ? .degrees(0.0) : .degrees(animationAmountWrong), axis: (x: 0, y: 0, z: 1))
+                    .opacity(number != correctAnswer ? opacityAmount : 1)
                 }
                 
                 Text("\(score) points")
@@ -89,6 +106,10 @@ struct ContentView: View {
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0 ... 2)
+        
+        animationAmount = 0.0
+        animationAmountWrong = 0.0
+        opacityAmount = 1.0
     }
 }
 
