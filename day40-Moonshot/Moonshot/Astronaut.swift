b8fe03a60efa35d787953c19d4514bfc2b8036a5
Michael Knepprath <mknepprath@gmail.com>
2020-08-19 00:15:07 -0400

    Add Day 40

---
+++ day40-Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Michael Knepprath on 8/18/20.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ day40-Moonshot/Moonshot/Mission.swift
@@ -0,0 +1,38 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Michael Knepprath on 8/18/20.
+//
+
+import Foundation
+
+struct CrewRole: Codable {
+    let name: String
+    let role: String
+}
+
+struct Mission: Codable, Identifiable {
+    let id: Int
+    let launchDate: Date?
+    let crew: [CrewRole]
+    let description: String
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+
+    var image: String {
+        "apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String {
+        if let launchDate = launchDate {
+            let formatter = DateFormatter()
+            formatter.dateStyle = .long
+            return formatter.string(from: launchDate)
+        } else {
+            return "N/A"
+        }
+    }
+}
