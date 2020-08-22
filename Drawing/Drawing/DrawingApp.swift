c18bfe82b6ee26510835793c2535c41fe554333a
Michael Knepprath <mknepprath@gmail.com>
2020-08-22 17:14:17 -0400

    Add Day 43

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Michael Knepprath on 8/22/20.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
