8aac68e4841f787a931d66edc3f9eee9063dd242
Michael Knepprath <mknepprath@gmail.com>
2020-08-29 19:24:01 -0400

    Add Day 49

---
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -0,0 +1,111 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Michael Knepprath on 8/29/20.
+//
+
+import SwiftUI
+
+class User: ObservableObject, Codable {
+    enum CodingKeys: CodingKey {
+        case name
+    }
+    
+    @Published var name = "Michael Knepprath"
+    
+    // Required
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+    }
+}
+
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
+//struct ContentView: View {
+//    @State var results = [Result]()
+//
+//    var body: some View {
+//        List(results, id: \.trackId) { item in
+//            VStack(alignment: .leading) {
+//                Text(item.trackName)
+//                    .font(.headline)
+//
+//                Text(item.collectionName)
+//            }
+//        }
+//        .onAppear(perform: loadData)
+//    }
+//
+//    func loadData() {
+//        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+//            print("Invalid URL")
+//            return
+//        }
+//
+//        let request = URLRequest(url: url)
+//
+//        URLSession.shared.dataTask(with: request) { data, response, error in
+//            if let data = data {
+//                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+//                    // we have good data – go back to the main thread
+//                    DispatchQueue.main.async {
+//                        // update our UI
+//                        self.results = decodedResponse.results
+//                    }
+//
+//                    // everything is good, so we can exit
+//                    return
+//                }
+//            }
+//
+//            // if we're still here it means there was a problem
+//            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+//        }.resume() // <- Frequently forgotten, and makes things not work.
+//    }
+//}
+
+struct ContentView: View {
+    @State var username = ""
+    @State var email = ""
+    
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
+    }
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
+            }
+            
+            Section {
+                Button("Create account") {
+                    print("Creating account...")
+                }
+            }
+            .disabled(disableForm)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Michael Knepprath on 8/29/20.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
