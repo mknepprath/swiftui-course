332c0d4b5d9991948b2182e57ba6f64a786b15cc
Michael Knepprath <mknepprath@gmail.com>
2020-09-05 21:26:24 -0400

    Add Day 56

---
+++ day53-Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Michael Knepprath on 9/1/20.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
